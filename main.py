ad1dd847d8b75e2ebcc6781ae84a8cd08457e7db
andreititussandu <andrei.titus.sandu@gmail.com>
2023-11-06 16:43:12 +0200

    endpoints configured returning top_topic_terms

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import string
-import tensorflow as tf
 import nltk
 import pandas as pd
 
@@ -19,11 +18,10 @@ nltk.download('stopwords')
 
 app = Flask(__name__)
 
-urls = [
-    "https://www.mayoclinic.org/drugs-supplements-creatine/art-20347591",
-    "https://www.infoworld.com/article/3204016/what-is-python-powerful-intuitive-programming.html",
-    "https://www.football365.com/news/summer-transfer-window-2023-most-expensive-players-biggest-deals"
-]
+dataset = pd.read_csv('website_classification.csv')
+df = dataset[['website_url', 'cleaned_website_text', 'Category']].copy()
+
+urls = dataset['website_url'].tolist()
 # Document list to be worked on
 your_documents = []
 
@@ -71,11 +69,11 @@ dictionary = Dictionary(your_documents)
 corpus = [dictionary.doc2bow(doc) for doc in your_documents]
 
 # Train the LDA model
-lda_model = LdaModel(corpus, id2word=dictionary, num_topics=3)
+lda_model = LdaModel(corpus, id2word=dictionary, num_topics=15)
 
 
-# Function to get topics and summary for a URL
-def get_topics_and_summary(url):
+# Function to get topics for a URL
+def get_topics(url):
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -109,8 +107,8 @@ def get_topics_and_summary(url):
         # top_topic_terms = lda_model.print_topic(top_topic)
 
         # Generate a summary
-        summary = ' '.join(relevant_words[:100])
-        return [top_topic[0], top_topic_terms, top_topic_strength, summary]
+        #summary = ' '.join(relevant_words[:100])
+        return top_topic_terms
         # return {
         #     "top_topic": top_topic[0],
         #     "top_topic_terms": top_topic_terms,
@@ -124,63 +122,37 @@ def get_topics_and_summary(url):
 @app.route('/', methods=['GET', 'POST'])
 def get_urls():
     try:
-        dataset = pd.read_csv(
-            '/Users/dennissaadeddin/Documents/Web Dev/Titus/topic_analysis_summarization/website_classification.csv')
-        df = dataset[['website_url', 'cleaned_website_text', 'Category']].copy()
-        urls = request.args.getlist('url')  # Use request.args to get a list of URLs
-
-        # print(df.head())
-        # print(pd.DataFrame(df.cleaned_website_text.unique()).values)
-        results = []
-        topic_summary_list = []
         if request.method == 'POST':
-            try:
-                print('it is a POST method')
-                topic_summary_list = get_topics_and_summary(urls[0])
-                # for url in urls:
-                #     get_topics_and_summary(url)
-                #     # results.append(result)
-                # return render_template('index.html')
-                return render_template('analyze.html', top_topic=topic_summary_list[0],
-                                       top_topic_terms=topic_summary_list[1], top_topic_strength=topic_summary_list[2],
-                                       summary=topic_summary_list[3])
-            except Exception as e:
-                return jsonify({"error": str(e)})
-        if request.method == 'GET':
-            print('it is a GET method')
+            print('it is a POST method')
+            url = request.form.get('url')
+            print(f"Extracted URL: {url}")
+            print(urls)
+            if url:
+                webpage_topic = get_topics(url)
+                return jsonify({"top_topic_terms": webpage_topic})
+                #return render_template('analyze.html', top_topic_terms=webpage_topic)
+            else:
+                return render_template('index.html')
+        else:
             return render_template('index.html')
-        return jsonify(results)
     except Exception as e:
         return jsonify({"error": str(e)})
 
 
-@app.route('/analyze', methods=['POST'])
-def analyze():
-    try:
-        dataset = pd.read_csv(
-            '/Users/dennissaadeddin/Documents/Web Dev/Titus/topic_analysis_summarization/website_classification.csv')
-        df = dataset[['website_url', 'cleaned_website_text', 'Category']].copy()
-        urls = request.args.getlist('url')  # Use request.args to get a list of URLs
-
-        # print(df.head())
-        # print(pd.DataFrame(df.cleaned_website_text.unique()).values)
-        results = []
-        topic_summary_list = []
-        if request.method == 'POST':
-            try:
-                print('it is a POST method')
-                topic_summary_list = get_topics_and_summary(urls[0])
-                # for url in urls:
-                #     get_topics_and_summary(url)
-                #     # results.append(result)
-                # return render_template('index.html')
-            except Exception as e:
-                return jsonify({"error": str(e)})
-        return render_template('analyze.html', top_topic=topic_summary_list[0],
-                               top_topic_terms=topic_summary_list[1], top_topic_strength=topic_summary_list[2],
-                               summary=topic_summary_list[3])
-    except Exception as e:
-        return jsonify({"error": str(e)})
+# @app.route('/analyze', methods=['POST'])
+# def analyze():
+#     try:
+#         dataset = pd.read_csv('website_classification.csv')
+#         if request.method == 'POST':
+#             print('it is a POST method')
+#             url = request.form['url']
+#             topic_summary_list = get_topics(url)
+#             return render_template('analyze.html', top_topic=topic_summary_list[0],
+#                                    top_topic_terms=topic_summary_list[1], top_topic_strength=topic_summary_list[2],
+#                                    summary=topic_summary_list[3])
+#         return jsonify({"error": "No URL specified"})
+#     except Exception as e:
+#         return jsonify({"error": str(e)})
 
 
 if __name__ == '__main__':
