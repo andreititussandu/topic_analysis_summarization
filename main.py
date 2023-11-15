a48df5e42a8c3f3b052eda7b3776b34e7a83d9c4
andreititussandu <andrei.titus.sandu@gmail.com>
2023-11-15 13:52:05 +0200

    cleansed dataset and configured model for each url

--- main.py
+++ main.py
@@ -11,25 +11,11 @@ import string
 import nltk
 import pandas as pd
 
-# print(tf.__version__)
-
 nltk.download('punkt')
 nltk.download('stopwords')
 
 app = Flask(__name__)
 
-dataset = pd.read_csv('website_classification.csv')
-df = dataset[['website_url', 'cleaned_website_text', 'Category']].copy()
-
-urls = dataset['website_url'].tolist()
-# Document list to be worked on
-your_documents = []
-
-stop_words = set(stopwords.words('english')).union({"-", "_", "'"})
-custom_stop_words = set(["-", "'", "_"])
-
-
-# Function to clean and preprocess text
 def preprocess_text(text):
     cleaned_text = re.sub(r'<.*?>', '', text)
     cleaned_text = re.sub(r'\n', ' ', cleaned_text)
@@ -39,85 +25,57 @@ def preprocess_text(text):
     relevant_words = [word for word in tokens if word not in stop_words]
     return relevant_words
 
-
-# Collect web page contents and preprocess
-def scraping(urls):
-    for url in urls:
+def create_lda_model(dataframe):
+    your_documents = []
+    for url in dataframe['website_url']:
         try:
             response = requests.get(url)
             soup = BeautifulSoup(response.content, 'html.parser')
-            paragraphs = soup.find_all('p')  # Split the text into paragraphs
+            paragraphs = soup.find_all('p')
             text_content = ""
             for paragraph in paragraphs:
                 text_content += paragraph.get_text() + " "
-
-            # Preprocess the text
             relevant_words = preprocess_text(text_content)
-
-            # Append the relevant words to your_documents
             your_documents.append(relevant_words)
         except Exception as e:
             print(f"Error processing URL {url}: {e}")
 
+    dictionary = Dictionary(your_documents)
+    corpus = [dictionary.doc2bow(doc) for doc in your_documents]
+    lda_model = LdaModel(corpus, id2word=dictionary, num_topics=len(dataframe['Category'].unique()))
+    return lda_model, dictionary
 
-scraping(urls)
+stop_words = set(stopwords.words('english')).union({"-", "_", "'"})
+custom_stop_words = set(["-", "'", "_"])
 
-# Create a Dictionary object from your documents
-dictionary = Dictionary(your_documents)
+dataset = pd.read_csv('website_classification_copy.csv')
+df = dataset[['website_url', 'cleaned_website_text', 'Category']].copy()
 
-# Convert the documents to bag-of-words format
-corpus = [dictionary.doc2bow(doc) for doc in your_documents]
+lda_model, dictionary = create_lda_model(df)
 
-# Train the LDA model
-lda_model = LdaModel(corpus, id2word=dictionary, num_topics=15)
-
-
-# Function to get topics for a URL
 def get_topics(url):
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
-        paragraphs = soup.find_all('p')  # Split the text into paragraphs
+        paragraphs = soup.find_all('p')
         text_content = ""
         for paragraph in paragraphs:
             text_content += paragraph.get_text() + " "
-
-        # Preprocess the text
         relevant_words = preprocess_text(text_content)
-
-        # Create a bag-of-words representation
         bow = dictionary.doc2bow(relevant_words)
-
-        # Get topics from the LDA model
         topics = lda_model.get_document_topics(bow)
-
-        # Sort topics by strength
         topics.sort(key=lambda x: x[1], reverse=True)
-
-        # Extract the top topic and its strength
         top_topic = topics[0]
-
-        # Get the terms associated with the top topic
-        top_topic_terms = lda_model.print_topic(0)  # or top_topic[0]
-
-        # Convert top_topic strength to a serializable data type (e.g., float64)
+        top_topic_terms = lda_model.print_topic(top_topic[0], len(dictionary))
         top_topic_strength = float(top_topic[1])
-
-        # Get the terms associated with the top topic
-        # top_topic_terms = lda_model.print_topic(top_topic)
-
-        # Generate a summary
-        #summary = ' '.join(relevant_words[:100])
-        return top_topic_terms
-        # return {
-        #     "top_topic": top_topic[0],
-        #     "top_topic_terms": top_topic_terms,
-        #     "top_topic_strength": top_topic_strength,
-        #     "summary": summary
-        # }
+        url_category = df[df['website_url'] == url]['Category'].values[0]
+        return {
+            "top_topic_terms": top_topic_terms,
+            "top_topic_strength": top_topic_strength,
+            "category": url_category
+        }
     except Exception as e:
         return {"error": str(e)}
-
 
 @app.route('/', methods=['GET', 'POST'])
 def get_urls():
@@ -126,34 +84,15 @@ def get_urls():
             print('it is a POST method')
             url = request.form.get('url')
             print(f"Extracted URL: {url}")
-            print(urls)
             if url:
                 webpage_topic = get_topics(url)
-                return jsonify({"top_topic_terms": webpage_topic})
-                #return render_template('analyze.html', top_topic_terms=webpage_topic)
+                return jsonify(webpage_topic)
             else:
                 return render_template('index.html')
         else:
             return render_template('index.html')
     except Exception as e:
         return jsonify({"error": str(e)})
-
-
-# @app.route('/analyze', methods=['POST'])
-# def analyze():
-#     try:
-#         dataset = pd.read_csv('website_classification.csv')
-#         if request.method == 'POST':
-#             print('it is a POST method')
-#             url = request.form['url']
-#             topic_summary_list = get_topics(url)
-#             return render_template('analyze.html', top_topic=topic_summary_list[0],
-#                                    top_topic_terms=topic_summary_list[1], top_topic_strength=topic_summary_list[2],
-#                                    summary=topic_summary_list[3])
-#         return jsonify({"error": "No URL specified"})
-#     except Exception as e:
-#         return jsonify({"error": str(e)})
-
 
 if __name__ == '__main__':
     app.run(debug=False)
